62103040d0d406767868da0e3ab1991c0741fcb2
Zhy149 <745476944@qq.com>
2019-01-20 21:20:15 +0800

    批改：批改王文军第三次作业

--- 3.js基础/3.js基础-王文军/JS/index.js
+++ 3.js基础/3.js基础-王文军/JS/index.js
@@ -23,34 +23,44 @@ function changeColor(){
 		}
 	}
 }
+
+// 批注：关于函数方法定义，建议将旧数组作为参数传入函数，并返回去重后的新数组
+
 //set去重
-function duplicateRemoval1(){
-	var list = [1,2,2,3,5,8,3,5];
-	var set = new Set(list);
-	var result = "";
-	for( var i of set)
-		result +=i+" "
-	alert(result);
+// 批注：new Set(arr)之后返回的是一个可遍历的对象，需要将可遍历的对象重新转化为数组
+function duplicateRemoval1(list){
+	// var list = [1,2,2,3,5,8,3,5];
+	var set = new Set(list)
+	var newArr = [...set]
+	return newArr
+	// var result = "";
+	// for( var i of set)
+	// 	result +=i+" "
+	// alert(result);
 }
 //数组下标去重
-function duplicateRemoval2(){
-	var list = [1,2,2,3,5,8,3,5];
+function duplicateRemoval2(list){
+	// var list = [1,2,2,3,5,8,3,5];
 	var result = [];
-	for(var i =0; i < list.length; i++){
+	for(var i = 0; i < list.length; i++){
 		if(list.indexOf(list[i]) == i)
 			result.push(list[i]);
 	}
-	alert(result);
+	return newArr
+	// alert(result);
 }
+
 //排序相邻去重
-function duplicateRemoval3(){
-	var list = [1,2,2,3,5,8,3,5];
-	list.sort();
+// 批注：arr.sort()排序的方法：默认排序顺序是根据字符串Unicode码点。
+function duplicateRemoval3(list){
+	// var list = [1,2,2,3,5,8,3,5];
+	list.sort((a, b) => a - b);
 	var result = [];
 	result.push(list[0]);
 	for(var i =1; i < list.length; i++){
 		if(list[i] != list[i-1])
 			result.push(list[i]);
 	}
-	alert(result);
+	// alert(result);
+	return result
 }
